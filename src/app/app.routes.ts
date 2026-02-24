8c54c9736270cf20a12d0bc403b2195ffd544f8b
Ahmed Belaid <elyesbelaid.2005@gmail.com>
2026-02-24 14:29:38 +0100

    set routes and header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'products' },
+    { path: "products", loadComponent: () => import('./components/products-grid/products-grid')},
+    { path: "wishlist", loadComponent: () => import('./components/wishlist/wishlist')},
+];
